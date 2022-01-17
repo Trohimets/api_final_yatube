1cefc04a7320dca618bdc28ad3bea158d486718b
Константин Трохимец <katrohimets@mail.ru>
2022-01-17 23:02:20 +0300

    monday

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,24 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from api.views import PostViewSet, GroupViewSet
+from api.views import CommentViewSet
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments/(?P<comment_id>\d+)',
+    CommentViewSet, basename='comment_detail'
+)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
